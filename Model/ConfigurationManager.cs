d687e4ef9473427c8c3cda2df82ec5fde109249f
Granfeldt <soren@granfeldt.dk>
2015-10-12 17:04:28 +0200

    Moved classes here

---
+++ Model/ConfigurationManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FIM.MARE
+{
+	public class ConfigurationManager : IDisposable
+	{
+		public void LoadSettingsFromFile(string Filename, ref Configuration configuration)
+		{
+			XmlSerializer serializer = new XmlSerializer(typeof(Configuration));
+			StreamReader textReader = new StreamReader(Filename);
+			configuration = (Configuration)serializer.Deserialize(textReader);
+			textReader.Close();
+			textReader.Dispose();
+		}
+		public void Dispose()
+		{
+		}
+	}
+}
